502e5856d02edd32e6bb17c1b362c6610377f92c
Keenan Shumard <keenan.shumard@gmail.com>
2024-11-27 17:40:07 -0600

    New year new Code

--- 2023/puzzles/day-3/day-3-a.ts
+++ 2023/puzzles/day-3/day-3-a.ts
@@ -3,7 +3,76 @@ import chalk from 'chalk';
 
 export async function day3a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let sum = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    const numbers = data[i].match(/\d+/g);
+    numbers && console.log(i + ': ' + hasDuplicates(numbers));
+    numbers &&
+      numbers.forEach((partNumber) => {
+        // console.log(partNumber + ': ' + isPartNumber(i, data, partNumber));
+        if (isPartNumber(i, data, partNumber)) {
+          sum += parseInt(partNumber);
+        }
+      });
+  }
+
+  return sum;
+}
+
+function hasDuplicates(array: string[]) {
+  var valuesSoFar = Object.create(null);
+  for (var i = 0; i < array.length; ++i) {
+    var value = array[i];
+    if (value in valuesSoFar) {
+      return true;
+    }
+    valuesSoFar[value] = true;
+  }
+  return false;
+}
+
+function isPartNumber(
+  lineNumber: number,
+  schematic: string[],
+  partNumber: string,
+) {
+  const partStartingIndex = schematic[lineNumber].indexOf(partNumber);
+  const partEndingIndex = partStartingIndex + partNumber.length;
+  //find x range
+  // const searchStartingX = partStartingIndex < 0 ? partStartingIndex - 1 : 0;
+  // const searchEndingX =
+  //   searchStartingX + partNumber.length < schematic[lineNumber].length
+  //     ? searchStartingX + partNumber.length +1
+  //     : schematic[lineNumber].length - 1;
+  const searchStartingX = partStartingIndex - 1;
+  const searchEndingX = searchStartingX + partNumber.length + 1;
+
+  //find y range
+  // const searchStartingY = lineNumber < 0 ? lineNumber - 1 : 0;
+  // const searchEndingY =
+  //   lineNumber < schematic.length - 2 ? lineNumber - 1 : lineNumber;
+  const searchStartingY = lineNumber - 1;
+  const searchEndingY = lineNumber + 1;
+
+  // console.log('X: ' + searchStartingX + '=>' + searchEndingX);
+  // console.log('Y: ' + searchStartingY + '=>' + searchEndingY);
+  for (let i = searchStartingY; i <= searchEndingY; i++) {
+    for (let j = searchStartingX; j <= searchEndingX; j++) {
+      //check if valid index
+      if (j in schematic && i in schematic[j].split('')) {
+        // console.log('Testing ' + i + ' ' + j);
+        if (
+          schematic[i] &&
+          schematic[i][j] &&
+          /[^\w\s.]/.test(schematic[i][j])
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 }
 
 const answer = await day3a();
---
+++ README.md
@@ -0,0 +1,7 @@
+# Keenan's Advent of Code repo
+
+## To setup for a new year
+
+1. Open console to base folder
+2. run `npx create-ts-aoc-starter`
+3. Rename new folder to year
